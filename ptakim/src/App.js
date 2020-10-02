443d1bbd868eab4e2b4e61d4735723b97b4ad883
swichi <lightsean10@gmail.com>
2020-10-02 18:47:32 +0300

    aa

--- ptakim/src/App.js
+++ ptakim/src/App.js
@@ -6,7 +6,7 @@ import { PubNubProvider} from 'pubnub-react';
 const pubnub = new PubNub({
     publishKey: 'pub-c-c2d54f38-8db8-44b8-8065-59f3587affd8',
     subscribeKey: 'sub-c-70cd517e-001e-11eb-ae2d-56dc81df9fb5',
-    uuid: '123456',
+    // uuid: 'sean the man',
     presenceTimeout: 20,
 });
 
--- ptakim/src/views/MainMenu.js
+++ ptakim/src/views/MainMenu.js
@@ -9,6 +9,8 @@ const our_channels = ['yeah'];
 function MainMenu() {
     const pubnub = usePubNub();
     const [connected, setConnected] = useState([])
+    const [name, setName] = useState('');
+    const [game_joined, setGameJoined] = useState('');
 
     const initializeConnected = () => {
         pubnub.hereNow(
@@ -26,37 +28,92 @@ function MainMenu() {
     }
 
     const activate = async () => {
-        await pubnub.addListener({
-            presence: function(event) {
-                let action = event.action;
-                let channelName = event.channel;
-                let occupancy = event.occupancy;
-                let eventTimetoken = event.timetoken;
-                let occupantUUID = event.uuid;
-                let state = event.state;
-                let subscribeManager = event.subscription;
-                console.log(`accepting event of ${action} with uuid ${occupantUUID}`)
-                if(action === 'join'){
-                    setConnected(connected => [...connected, occupantUUID]);
-                }else if (action === 'leave'){
-                    setConnected(connected => connected.filter((uuid) => (uuid !== occupantUUID)))
-                }
-            }
-        });
-        await pubnub.subscribe({channels: our_channels, withPresence: true});
-        await initializeConnected();
+        // await pubnub.addListener({
+        //     presence: function(event) {
+        //         let action = event.action;
+        //         let channelName = event.channel;
+        //         let occupancy = event.occupancy;
+        //         let eventTimetoken = event.timetoken;
+        //         let occupantUUID = event.uuid;
+        //         let state = event.state;
+        //         let subscribeManager = event.subscription;
+        //         console.log(`accepting event of ${action} with uuid ${occupantUUID}`)
+        //         if(action === 'join'){
+        //             setConnected(connected => [...connected, occupantUUID]);
+        //         }else if (action === 'leave' || action === 'timeout'){
+        //             setConnected(connected => connected.filter((uuid) => (uuid !== occupantUUID)))
+        //         }
+        //     }
+        // });
+        // await pubnub.subscribe({channels: our_channels, withPresence: true});
+        // await initializeConnected();
     }
 
     useEffect(() => {
         activate();
     }, []);
 
-    return (
+    const hostGame = async () => {
+        await pubnub.setUUID(name);
+        let channel_name = `${name}_game`;
+        await pubnub.subscribe({channels: [channel_name], withPresence: true});
+        alert(`hosted ${channel_name} successfully!`);
+    }
+
+    const joinGame = () => {
+        setGameJoined('Enter game id');
+    }
+
+
+    const check = () => {
+        pubnub.hereNow(
+            {
+                channels: [game_joined]
+            },
+            function (status, response) {
+                let occupants_list = response.channels[game_joined].occupants.map((occ) => {
+                    return occ.uuid
+                })
+                console.log(occupants_list)
+            }
+        )
+    }
+
+    const joinGamePressed = async () => {
+        await pubnub.setUUID(name);
+        await pubnub.subscribe({channels: [game_joined], withPresence: true});
+        await pubnub.hereNow(
+            {
+                channels: [game_joined]
+            },
+            function (status, response) {
+                let occupants_list = response.channels[game_joined].occupants.map((occ) => {
+                    return occ.uuid
+                })
+                console.log(occupants_list)
+            }
+        );
+        // alert(`joiend ${game_joined} successfully!`);
+    }
+
+    let comp = (
         <div>
-            <h1>hi :(</h1>
-            {connected.map((uuid, idx) => {
-                return <h1 key={idx}>{uuid}</h1>
-            })}
+            <input value={game_joined} onChange={(e) => {setGameJoined(e.target.value)}}/>
+            <button onClick={joinGamePressed}>JOIN</button>
+        </div>
+
+    )
+
+    return (
+        <div style={{display: 'flex', flexDirection:'column' ,justifyContent: 'center', alignItems:'space-between', minHeight: 250, minWidth: 250,border:'solid'}}>
+            <div style={{display: 'flex', flexDirection:'row', flexGrow:0, justifyContent:'center'}}>
+                <h3>Enter your name -> </h3>
+                <input value={name} onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <button style={{flexGrow: 0}} onClick={hostGame}>Host</button>
+            <button onClick={joinGame}>Join</button>
+            {game_joined ? (comp) : null}
+            <button onClick={check}>CHECK</button>
         </div>
     );
 }
